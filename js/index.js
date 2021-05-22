b480d519ea2865a707725783811fdeacbe58588b
vikrm <abhishek8587singh@gmail.com>
2021-05-22 18:14:59 +0530

    aos hovers

--- js/index.js
+++ js/index.js
@@ -1,20 +1,18 @@
-function expand(){
-  let navbar = document.querySelector('.navigation');
-  let bar = document.querySelector('.bar');
-  navbar.classList.toggle('expand');
-  bar.classList.toggle('show');
-
+function expand() {
+  let navbar = document.querySelector(".navigation");
+  let bar = document.querySelector(".bar");
+  navbar.classList.toggle("expand");
+  bar.classList.toggle("show");
 }
-
 
 //swiper initialize
 
-const swiperMain = new Swiper('.swiperMain', {
-   // Optional parameters
-   direction: 'vertical',
-   mousewheel: true,
-   speed:1500,
-   spaceBetween:0,
+const swiperMain = new Swiper(".swiperMain", {
+  // Optional parameters
+  direction: "vertical",
+  mousewheel: true,
+  speed: 1500,
+  spaceBetween: 0,
   //  effect: "slide",
   //  coverflowEffect: {
   //   rotate: 50,
@@ -23,82 +21,75 @@ const swiperMain = new Swiper('.swiperMain', {
   //   modifier: 1,
   //   slideShadows: true
   // },
-   loop: true,
-   keyboard: {
+  loop: true,
+  keyboard: {
     enabled: true,
   },
- 
-   // If we need pagination
-   pagination: {
-     el: '.mainPagination',
-     clickable: true,
-     
-   },
- 
-   // Navigation arrows
-   navigation: {
-     nextEl: '.swiper-button-next',
-     prevEl: '.swiper-button-prev',
-   },
- 
-   // And if we need scrollbar
-   scrollbar: {
-     el: '.swiper-scrollbar',
-   },
- });
 
+  // If we need pagination
+  pagination: {
+    el: ".mainPagination",
+    clickable: true,
+  },
 
+  // Navigation arrows
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
 
+  // And if we need scrollbar
+  scrollbar: {
+    el: ".swiper-scrollbar",
+  },
+});
 
-      const homeSlider = new Swiper('.homeSlider', {
-   // Optional parameters
-   direction: 'horizontal',
-   slidesPerView: 1,
-   spaceBetween: 30,
-   centeredSlides: true,
-   loop: true,
-   keyboard: {
+const homeSlider = new Swiper(".homeSlider", {
+  // Optional parameters
+  direction: "horizontal",
+  slidesPerView: 1,
+  spaceBetween: 30,
+  centeredSlides: true,
+  loop: true,
+  keyboard: {
     enabled: true,
   },
- 
-   // If we need pagination
-   pagination: {
-     el: '.homePagination',
-     clickable:true,
-   },
-   
-   // Navigation arrows
-   navigation: {
-     nextEl: '.swiper-button-next',
-     prevEl: '.swiper-button-prev',
-   },
- 
-   // And if we need scrollbar
-   scrollbar: {
-     el: '.swiper-scrollbar',
-   },
- });
 
+  // If we need pagination
+  pagination: {
+    el: ".homePagination",
+    clickable: true,
+  },
 
+  // Navigation arrows
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
 
+  // And if we need scrollbar
+  scrollbar: {
+    el: ".swiper-scrollbar",
+  },
+});
 
-      const swiper2 = new Swiper('.swiper2', {
-   // Optional parameters
-   direction: 'horizontal',
-   slidesPerView: 1,
-   spaceBetween: 30,
-   centeredSlides: true,
-   loop: true,
-   keyboard: {
+const swiper2 = new Swiper(".swiper2", {
+  // Optional parameters
+  direction: "horizontal",
+  slidesPerView: 1,
+  spaceBetween: 30,
+  centeredSlides: true,
+  loop: true,
+  keyboard: {
     enabled: true,
   },
- 
-   // If we need pagination
-   pagination: {
-     el: '.portfolioPagination',
-     clickable:true,
-   },
-    
+
+  // If we need pagination
+  pagination: {
+    el: ".portfolioPagination",
+    clickable: true,
+  },
+
   breakpoints: {
     576: {
       slidesPerView: 1,
@@ -114,40 +105,34 @@ const swiperMain = new Swiper('.swiperMain', {
       slidesPerView: 4,
       spaceBetween: 20,
       centeredSlides: false,
-
     },
   },
-   // Navigation arrows
-   navigation: {
-     nextEl: '.swiper-button-next',
-     prevEl: '.swiper-button-prev',
-   },
- 
-   // And if we need scrollbar
-   scrollbar: {
-     el: '.swiper-scrollbar',
-   },
- });
+  // Navigation arrows
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
 
+  // And if we need scrollbar
+  scrollbar: {
+    el: ".swiper-scrollbar",
+  },
+});
 
+const mySwiper = new Swiper(".mySwiper", {
+  // Optional parameters
+  direction: "horizontal",
+  slidesPerView: 1,
+  spaceBetween: 30,
+  loop: true,
 
+  // If we need pagination
+  pagination: {
+    el: ".testimonialPagination",
+    clickable: true,
+  },
 
-
-
-      const mySwiper= new Swiper('.mySwiper', {
-   // Optional parameters
-   direction: 'horizontal',
-   slidesPerView: 1,
-   spaceBetween: 30,
-   loop: true,
- 
-   // If we need pagination
-   pagination: {
-     el: '.testimonialPagination',
-     clickable: true,
-   },
- 
-   breakpoints: {
+  breakpoints: {
     576: {
       slidesPerView: 1,
       spaceBetween: 20,
@@ -162,90 +147,76 @@ const swiperMain = new Swiper('.swiperMain', {
       slidesPerView: 3,
       spaceBetween: 20,
       centeredSlides: false,
-
     },
   },
-   // Navigation arrows
-   navigation: {
-     nextEl: '.swiper-button-next',
-     prevEl: '.swiper-button-prev',
-   },
- 
-   // And if we need scrollbar
-   scrollbar: {
-     el: '.swiper-scrollbar',
-   },
- });
+  // Navigation arrows
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
 
+  // And if we need scrollbar
+  scrollbar: {
+    el: ".swiper-scrollbar",
+  },
+});
 
+const serviceSwiper = new Swiper(".serviceSwiper", {
+  // Optional parameters
+  direction: "horizontal",
+  slidesPerView: 1,
+  spaceBetween: 30,
+  centeredSlides: true,
+  loop: false,
 
-
-
-
-
-
-      const serviceSwiper = new Swiper('.serviceSwiper', {
-   // Optional parameters
-   direction: 'horizontal',
-   slidesPerView: 1,
-   spaceBetween: 30,
-   centeredSlides: true,
-   loop: false,
- 
-   // If we need pagination
-   pagination: {
-     el: '.servicePagination',
-     clickable: true,
-   },
+  // If we need pagination
+  pagination: {
+    el: ".servicePagination",
+    clickable: true,
+  },
   breakpoints: {
     576: {
       slidesPerView: 2,
       spaceBetween: 20,
-   centeredSlides: true,
-   navigation: {
-    nextEl: '.swiper-button-next',
-    prevEl: '.swiper-button-prev',
-  },
-
+      centeredSlides: true,
+      navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+      },
     },
     768: {
       slidesPerView: 2,
       spaceBetween: 40,
       centeredSlides: false,
-
     },
     992: {
       slidesPerView: 3,
       spaceBetween: 10,
       centeredSlides: false,
-
     },
   },
- 
-   // Navigation arrows
-   
- 
-   // And if we need scrollbar
-   scrollbar: {
-     el: '.swiper-scrollbar',
-   },
- });
 
+  // Navigation arrows
 
+  // And if we need scrollbar
+  scrollbar: {
+    el: ".swiper-scrollbar",
+  },
+});
 
 //  mouse move Animation
 
-document.body.addEventListener('mousemove',(e)=>{
+document.body.addEventListener("mousemove", (e) => {
+  var footer = document.querySelector(".footer-bg");
+  var card = document.querySelector("#tilt-img");
 
-    var footer = document.querySelector('.footer-bg')
-    var card = document.querySelector('#tilt-img') ;
+  var clX = e.clientX;
+  var clY = e.clientY;
 
-   var clX = e.clientX;
-   var clY = e.clientY;
-
-    card.style.transform = `translate(${clX*3/100}px , ${clY*3/100}px)` ;
-    footer.style.backgroundPosition = `${clX*3/100}px , ${clY*7/100}px` ;
-
-
-
-})
+  card.style.transform = `translate(-${(clX * 3) / 100}px , -${
+    (clY * 3) / 100
+  }px)`;
+  footer.style.backgroundPosition = `${(clX * 3) / 100}px , ${
+    (clY * 7) / 100
+  }px`;
+});
